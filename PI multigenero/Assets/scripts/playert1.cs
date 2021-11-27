94d18e70f50b9d15ff9e587bf8b7f01c04140911
Hiago Luiz <ogaih007@gmail.com>
2021-11-27 17:27:06 -0300

    consertos

--- PI multigenero/Assets/scripts/playert1.cs
+++ PI multigenero/Assets/scripts/playert1.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class playert1 : MonoBehaviour
 {
+    public float Speed;
+
+    private Rigidbody2D rig;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+      rig = GetComponent<Rigidbody2D>();  
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void movelr()
+    {
+        if(Input.GetKey("D"))
+        {
+            
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            
+        }
     }
 }
