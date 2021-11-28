8d2d66dde87c8a562f53e5b3ae2f4ddf550a192f
Hiago Luiz <ogaih007@gmail.com>
2021-11-28 01:18:48 -0300

    dialogo consertado

--- PI multigenero/Assets/scripts/DialogControl.cs
+++ PI multigenero/Assets/scripts/DialogControl.cs
@@ -13,12 +13,44 @@ public class DialogControl : MonoBehaviour
 
     [Header("Settings")]
     public float typingSpeed;
+    private string[] sentences;
+    private int index;
 
-    public void Speech(Sprite p, string txt, string actorName)
+    public void Speech(Sprite p, string[] txt, string actorName)
     {
         dialogueObj.SetActive(true);
         profile.sprite = p;
-        speechText.text = txt;
+        sentences = txt;
         actorNameText.text = actorName;
+        StartCoroutine(TypeSentence());
+    }
+
+    IEnumerator TypeSentence()
+    {
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+            speechText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void NextSentence()
+    {
+        if(speechText.text == sentences[index])
+        {
+            //ainda ha textos
+            if(index < sentences.Length - 1)
+            {
+                index++;
+                speechText.text = "";
+                StartCoroutine(TypeSentence());
+            }
+            else//lido quando acaba os textos
+            {
+                speechText.text = "";
+                index = 0;
+                dialogueObj.SetActive(false);
+            }
+        }
     }
 }
