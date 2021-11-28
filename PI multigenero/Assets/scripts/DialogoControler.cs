ab7b068e6b3c1ed0b7c7021f583df1287fa1c33d
Hiago Luiz <ogaih007@gmail.com>
2021-11-27 21:16:28 -0300

    tutorial inacabado

--- PI multigenero/Assets/scripts/DialogoControler.cs
+++ PI multigenero/Assets/scripts/DialogoControler.cs
@@ -12,11 +12,43 @@ public class DialogoControler : MonoBehaviour
 
     public float txtspeed;
 
-    public void fala(Sprite p, string fal, string nom)
+    private string[] paginas;
+    private int index;
+
+    public void fala(Sprite p, string[] fal, string nom)
     {
         dialogoobj.SetActive(true);
         imjperfil.sprite = p;
-        txtfala.text = fal;
+        paginas = fal;
         txtnome.text = nom;
+        StartCoroutine(TypeSentence());
+    }
+
+    IEnumerator TypeSentence()
+    {
+        foreach (char frases in paginas[index].ToCharArray())
+        {
+            txtfala.text += frases;
+            yield return new WaitForSeconds(txtspeed);
+        }
+    }
+
+    public void proxpagina()
+    {
+        if(txtfala.text == paginas[index])
+        {
+            if(index < paginas.Length - 1)
+            {
+                index ++;
+                txtfala.text = " ";
+                StartCoroutine(TypeSentence());
+            }
+            else
+            {
+                txtfala.text = " ";
+                index = 0;
+                dialogoobj.SetActive(false);
+            }
+        }
     }
 }
--- PI multigenero/Assets/scripts/DialogoTutorial.cs
+++ PI multigenero/Assets/scripts/DialogoTutorial.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class DialogoTutorial : MonoBehaviour
 {
     public Sprite perfil;
-    public string falas;
+    public string[] falas;
     public string nome;
 
+    public LayerMask playerlayer;
+    public float radios;
+
+    public float movet;
+    private float timer;
+
     private DialogoControler DC;
+
+    bool onradios;
 
     void Start() 
     {
@@ -17,6 +25,42 @@ public class DialogoTutorial : MonoBehaviour
 
     public void mostrartut()
     {
+        Collider2D perto = Physics2D.OverlapCircle(transform.position, radios, playerlayer);
+        
+        if(perto != null)
+        {
+            onradios = true;
+        }
+        else
+        {
+            onradios = false;
+        }
+    }
 
+    void Update() 
+    {
+        timer += Time.deltaTime;
+        
+        if(timer >= movet)
+        {
+            timer = 0f;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(onradios)
+            {
+                DC.fala(perfil, falas, nome);
+            }
+        }
+    }
+
+    void FixedUpdate() 
+    {
+        mostrartut();    
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.DrawWireSphere(transform.position, radios);
     }
 }
