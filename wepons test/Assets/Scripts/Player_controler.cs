ee3b74d745a9612ba09a3a059de77a86207844a8
ogaiH007 <ogaih007@gmail.com>
2022-02-12 16:01:57 -0300

    Dummy Add
    adicionado sprite do Dummy de teste da espada

--- wepons test/Assets/Scripts/Player_controler.cs
+++ wepons test/Assets/Scripts/Player_controler.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player_controler : MonoBehaviour
 {
+    public float speed;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,13 @@ public class Player_controler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+      Move();  
+    }
+
+    void Move()
+    {
+        //movimento vertical
+        Vector3 movimento = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+        transform.position += movimento * Time.deltaTime * speed;
     }
 }
