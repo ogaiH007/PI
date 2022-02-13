4e666cc2f27f090f2a467c85e46bfea0e4553611
ogaiH007 <ogaih007@gmail.com>
2022-02-13 16:10:13 -0300

    Revert "dummy configurado"
    This reverts commit c6f928c71fd2707cd1d58d7de972c70f8a290a78.

--- wepons test/Assets/Scripts/Sword_controler.cs
+++ wepons test/Assets/Scripts/Sword_controler.cs
@@ -5,13 +5,12 @@ using UnityEngine;
 public class Sword_controler : MonoBehaviour
 {
     public float dano_causado;
+
     void OnTriggerEnter2D(Collider2D collider)
     {
         if(collider.gameObject.tag == "Enimy")
         {
-            Dummy_controler.instance.DanoRecebido = dano_causado;
-            Dummy_controler.instance.damtxt.SetActive(true);
-            Dummy_controler.instance.activetimer = true;
+
         }
     }
 }
