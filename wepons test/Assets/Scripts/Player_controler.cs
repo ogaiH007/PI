b16b4ca44df6d4dcbcf1bac3803a8cc4e2d86883
ogaiH007 <ogaih007@gmail.com>
2022-02-13 08:09:02 -0300

    otimização de codigo
    codigo otimizado removendo linhas desnescessarias

--- wepons test/Assets/Scripts/Player_controler.cs
+++ wepons test/Assets/Scripts/Player_controler.cs
@@ -1,48 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Player_controler : MonoBehaviour
 {
     public float speed;
     public float SwTime;
-
     public GameObject Sword;
-
     private bool activetimer;
     private float timerSw;
 
-    // Start is called before the first frame update
     void Start()
     {
 
     }
-
-    // Update is called once per frame
     void Update()
     {
-      Move();  
-      Wepon();
-
-      if(activetimer)
-      {
-          timerSw += Time.deltaTime;
-
-          if(timerSw >= SwTime)
-          {
-              Sword.SetActive(false);
-              timerSw = 0f;
-          }
-      }
+        Move();  
+        Wepon();
+        TimerControler();
     }
-
     void Move()
     {
-        //movimento vertical
         Vector3 movimento = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
         transform.position += movimento * Time.deltaTime * speed;
     }
-
    void Wepon()
     {
         if(Input.GetButtonDown("Fire1"))
@@ -50,9 +31,18 @@ public class Player_controler : MonoBehaviour
             Sword.SetActive(true);
             activetimer = true;
         }
-        /*if(Input.GetButtonUp("Fire1"))
+    }
+    void TimerControler()
+    {
+        if(activetimer)
         {
-            Sword.SetActive(false);
-        }*/
+            timerSw += Time.deltaTime;
+
+            if(timerSw >= SwTime)
+            {
+                Sword.SetActive(false);
+                timerSw = 0f;
+            }
+        }
     }
 }
--- wepons test/Assets/Scripts/Sword_controler.cs
+++ wepons test/Assets/Scripts/Sword_controler.cs
@@ -4,19 +4,7 @@ using UnityEngine;
 
 public class Sword_controler : MonoBehaviour
 {
-    public float dano_causado;     
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public float dano_causado;
 
     void OnTriggerEnter2D(Collider2D collider)
     {
