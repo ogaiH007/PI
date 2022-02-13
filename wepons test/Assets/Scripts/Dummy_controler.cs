c6f928c71fd2707cd1d58d7de972c70f8a290a78
ogaiH007 <ogaih007@gmail.com>
2022-02-13 14:38:31 -0300

    dummy configurado

--- wepons test/Assets/Scripts/Dummy_controler.cs
+++ wepons test/Assets/Scripts/Dummy_controler.cs
@@ -1,15 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Dummy_controler : MonoBehaviour
 {
+    public float timerdantxt;
+    public float DanoRecebido;
+    public GameObject damtxt;   
+    private float timer;
+    public bool activetimer;
+    public Text DanoRec;
+    public static Dummy_controler instance;
     void Start()
     {
-        
+        instance = this;
     }
     void Update()
     {
-        
+        TimerControler();
+        DanoRec.text = DanoRecebido.ToString();
+    }
+    void TimerControler()
+    {
+        if(activetimer)
+        {
+            timer += Time.deltaTime;
+
+            if(timer >= timerdantxt)
+            {
+                damtxt.SetActive(false);
+                timer = 0f;
+            }
+        }
     }
 }
