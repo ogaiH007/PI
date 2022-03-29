c432822f5d6a8635fb302e8550ddabe7ed16f453
ogaiH007 <ogaih007@gmail.com>
2022-03-29 14:23:32 -0300

    codigos animação movidos para script
    animações espada não funcionando

--- Jorge Final Developer Project/Assets/scripts/Player/Player_animations.cs
+++ Jorge Final Developer Project/Assets/scripts/Player/Player_animations.cs
@@ -4,15 +4,106 @@ using UnityEngine;
 
 public class Player_animations : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int idlepos;
+    private Animator anim;
+
+    public Player_animations instance;
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        instance = this;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        MoveAnims();
+    }
+
+    void MoveAnims()
+    {
+        anim.SetInteger("idle_walk", idlepos);
+
+        if(Input.GetAxis("Vertical") > 0)
+        {
+            idlepos = 5;
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        }
+        if(Input.GetAxis("Vertical") < 0)
+        {
+            idlepos = 4;
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        }
+        if(Input.GetAxis("Horizontal") > 0)
+        {
+            idlepos = 6;
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
+        }
+        if(Input.GetAxis("Horizontal") < 0)
+        {
+            idlepos = 6;
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        }
+
+        if(Input.GetAxis("Horizontal") == 0)
+        {
+            if(idlepos == 6)
+            {
+                idlepos = 3;
+            }
+        }
+        if(Input.GetAxis("Vertical") == 0)
+        {
+            if(idlepos == 4)
+            {
+                idlepos = 1;
+            }
+            else if(idlepos == 5)
+            {
+                idlepos = 2;
+            }
+        }
+    }
+
+    void WeponAnims()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            if(Input.GetAxis("Vertical") < 0)
+            {
+                anim.SetBool("Sword_donw", true);
+            }
+            if(Input.GetAxis("Vertical") > 0)
+            {
+                anim.SetBool("Sword_up", true);
+            }
+            if(Input.GetAxis("Horizontal") > 0)
+            {
+                anim.SetBool("Sword_side", true);
+                transform.eulerAngles = new Vector3(0f, 0f, 0f);
+            }
+            if(Input.GetAxis("Horizontal") < 0)
+            {
+                anim.SetBool("Sword_side", true);
+                transform.eulerAngles = new Vector3(0f, 180f, 0f);
+            }
+            
+            if(Input.GetAxis("Vertical") == 0)
+            {
+                if(idlepos == 1)
+                {
+                    anim.SetBool("Sword_donw", true);
+                }
+                else if(idlepos == 2)
+                {
+                    anim.SetBool("Sword_up", true);
+                }
+            }
+            if(Input.GetAxis("Horizontal") == 0)
+            {
+                if(idlepos == 3)
+                {
+                    anim.SetBool("Sword_side", true);
+                }
+            }
+        }
     }
 }
--- Jorge Final Developer Project/Assets/scripts/Player/Player_controler.cs
+++ Jorge Final Developer Project/Assets/scripts/Player/Player_controler.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Player_controler : MonoBehaviour
 {
-    public int idlepos;
     public float speed;
     public float SwTime;
     public GameObject Sword;
@@ -14,7 +13,7 @@ public class Player_controler : MonoBehaviour
     private float timerSw;
 
     private Animator anim;
-
+    
     void Start()
     {
         anim = GetComponent<Animator>();
@@ -32,51 +31,23 @@ public class Player_controler : MonoBehaviour
         {
             Vector3 movimento = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
             transform.position += movimento * Time.deltaTime * speed;
-            anim.SetInteger("idle_walk", idlepos); 
         }
-        
+
         if(Input.GetAxis("Vertical") > 0)
         {
-            idlepos = 5;
-            transform.eulerAngles = new Vector3(0f, 0f, 0f);
             Sword.transform.eulerAngles = new Vector3(0f, 0f, 0f);
         }
         if(Input.GetAxis("Vertical") < 0)
         {
-            idlepos = 4;
-            transform.eulerAngles = new Vector3(0f, 0f, 0f);
             Sword.transform.eulerAngles = new Vector3(0f, 0f, 180f);
         }
         if(Input.GetAxis("Horizontal") > 0)
         {
-            idlepos = 6;
-            transform.eulerAngles = new Vector3(0f, 180f, 0f);
             Sword.transform.eulerAngles = new Vector3(0f, 0f, -90f);
         }
         if(Input.GetAxis("Horizontal") < 0)
         {
-            idlepos = 6;
-            transform.eulerAngles = new Vector3(0f, 0f, 0f);
             Sword.transform.eulerAngles = new Vector3(0f, 0f, 90f);
-        }
-
-        if(Input.GetAxis("Horizontal") == 0)
-        {
-            if(idlepos == 6)
-            {
-                idlepos = 3;
-            }
-        }
-        if(Input.GetAxis("Vertical") == 0)
-        {
-            if(idlepos == 4)
-            {
-                idlepos = 1;
-            }
-            else if(idlepos == 5)
-            {
-                idlepos = 2;
-            }
         }
     }
    void Wepon()
@@ -84,48 +55,8 @@ public class Player_controler : MonoBehaviour
         if(Input.GetButtonDown("Fire1") && !activetimer)
         {
             usingwepon = true;
-            Sword.SetActive(true);
             activetimer = true;
-            //anim.SetBool("Sword_donw", true);
-            if(Input.GetAxis("Vertical") < 0)
-            {
-                anim.SetBool("Sword_donw", true);
-            }
-            if(Input.GetAxis("Vertical") > 0)
-            {
-                anim.SetBool("Sword_up", true);
-            }
-            if(Input.GetAxis("Horizontal") > 0)
-            {
-                anim.SetBool("Sword_side", true);
-                transform.eulerAngles = new Vector3(0f, 0f, 0f);
-            }
-            if(Input.GetAxis("Horizontal") < 0)
-            {
-                anim.SetBool("Sword_side", true);
-                transform.eulerAngles = new Vector3(0f, 180f, 0f);
-            }
-            
-            if(Input.GetAxis("Vertical") == 0)
-            {
-                if(idlepos == 1)
-                {
-                    anim.SetBool("Sword_donw", true);
-                }
-                else if(idlepos == 2)
-                {
-                    anim.SetBool("Sword_up", true);
-                }
-            }
-            if(Input.GetAxis("Horizontal") == 0)
-            {
-                if(idlepos == 3)
-                {
-                    anim.SetBool("Sword_side", true);
-                }
-            }
         }
-        
     }
     void TimerControler()
     {
@@ -135,11 +66,11 @@ public class Player_controler : MonoBehaviour
 
             if(timerSw >= SwTime)
             {
-                timerSw = 0f;
-                Sword.SetActive(false);
                 anim.SetBool("Sword_donw", false);
                 anim.SetBool("Sword_up", false);
                 anim.SetBool("Sword_side", false);
+                timerSw = 0f;
+                Sword.SetActive(false);
                 activetimer = false;
                 usingwepon = false;
             }
