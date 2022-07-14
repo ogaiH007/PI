958289aae4dbda9f53960cca69200e8ae9f46030
Hiago Luiz <ogaih007@gmail.com>
2022-07-14 19:38:38 -0300

    Codigo da espada Quase atualizado

--- Jorge Final Developer Project/Assets/scripts/Player/Sword_controler.cs
+++ Jorge Final Developer Project/Assets/scripts/Player/Sword_controler.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class Sword_controler : MonoBehaviour
 {
     public float dano_causado;
+    private GameObject ProxEnimy;
 
     void OnTriggerEnter2D(Collider2D collider)
     {
         if(collider.gameObject.tag == "Enimy")
         {
+            ProxEnimy = GameObject.FindGameObjectWithTag("Enimy");
+            Destroy(ProxEnimy);
+            
             /*Dummy_controler.DumCon.totalDam = dano_causado;
             Dummy_controler.DumCon.DamTXT.SetActive(true);
             Dummy_controler.DumCon.activetimer = true;*/
