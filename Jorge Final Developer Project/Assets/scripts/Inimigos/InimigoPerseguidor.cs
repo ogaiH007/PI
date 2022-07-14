9c516e045fa904ec7f413d32161d5e3c17fa414a
Hiago Luiz <ogaih007@gmail.com>
2022-07-14 19:58:47 -0300

    problemas tecnicos ignorar

--- Jorge Final Developer Project/Assets/scripts/Inimigos/InimigoPerseguidor.cs
+++ Jorge Final Developer Project/Assets/scripts/Inimigos/InimigoPerseguidor.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InimigoPerserguidor : MonoBehaviour
+public class InimigoPerseguidor : MonoBehaviour
 {
     private Transform posPlayer;
 
