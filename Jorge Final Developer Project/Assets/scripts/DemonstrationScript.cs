b486e4aba78cb0f2bbd19838e686ca28d273abed
Hiago Luiz <ogaih007@gmail.com>
2022-09-22 11:23:26 -0300

    Alguns testes bobos (Ponto seguro de Branch)

--- Jorge Final Developer Project/Assets/scripts/DemonstrationScript.cs
+++ Jorge Final Developer Project/Assets/scripts/DemonstrationScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DemonstrationScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float speed;
     void Start()
     {
         
@@ -13,6 +14,7 @@ public class DemonstrationScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 movimento = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+        transform.position += movimento * Time.deltaTime * speed;
     }
 }
