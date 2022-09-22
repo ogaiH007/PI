54911831a96ef16b9827353d3e676fec9219bccf
Hiago Luiz <ogaih007@gmail.com>
2022-09-22 12:50:46 -0300

    Doc Update

--- Jorge Final Developer Project/Assets/scripts/DemonstrationScript.cs
+++ Jorge Final Developer Project/Assets/scripts/DemonstrationScript.cs
@@ -8,11 +8,14 @@ public class DemonstrationScript : MonoBehaviour
     public float speed;
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
+    {
+        Move();
+    }
+    void Move()
     {
         Vector3 movimento = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
         transform.position += movimento * Time.deltaTime * speed;
