5b44da7f49e3ca2fd7aa3fed9e020157697720bc
Hiago Luiz <ogaih007@gmail.com>
2022-10-11 22:06:52 -0300

    Finalizado Capitulo 4.3.2 do Documento

--- Jorge Final Developer Project/Assets/scripts/DemonstrationScript.cs
+++ Jorge Final Developer Project/Assets/scripts/DemonstrationScript.cs
@@ -4,20 +4,47 @@ using UnityEngine;
 
 public class DemonstrationScript : MonoBehaviour
 {
-    // Start is called before the first frame update
     public float speed;
-    void Start()
-    {
-    }
-
+    public GameObject Espada;
     // Update is called once per frame
     void Update()
     {
         Move();
+        Wepon();
     }
     void Move()
     {
         Vector3 movimento = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
         transform.position += movimento * Time.deltaTime * speed;
+    }
+    void Wepon()
+    {
+        if (Input.GetAxis("Vertical") < 0 && Input.GetAxis("Horizontal") == 0)
+        {
+            Espada.transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        }
+        if (Input.GetAxis("Horizontal") > 0 && Input.GetAxis("Vertical") == 0)
+        {
+            Espada.transform.eulerAngles = new Vector3(0f, 0f, 90f);
+        }
+        if (Input.GetAxis("Vertical") > 0 && Input.GetAxis("Horizontal") == 0)
+        {
+            Espada.transform.eulerAngles = new Vector3(0f, 0f, 180f);
+        }
+        if (Input.GetAxis("Horizontal") < 0 && Input.GetAxis("Vertical") == 0)
+        {
+            Espada.transform.eulerAngles = new Vector3(0f, 0f, 270f);
+        }
+        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            StartCoroutine("WeponButon");
+        }
+    }
+    IEnumerator WeponButon()
+    {
+        Espada.SetActive(true);
+        yield return new WaitForSeconds(0.45f);
+        Espada.SetActive(false);
     }
 }
