c5406ebd4a16ad749742b3a4f83b02b534289b7a
Hiago Luiz <ogaih007@gmail.com>
2022-10-22 17:39:08 -0300

    Criando uma aleatoriedade na geração do inimigo

---
+++ Jorge Final Developer Project/Assets/scripts/Geraçãodeinimigos.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Geraçãodeinimigos : MonoBehaviour
+{
+    Vector3 LugarDoSpawn = new Vector3(0,0,0);
+    public GameObject inimigo;
+    public int minimoDeInimigos = 1, maximoDeInimigos = 10;//ajuste o minimo e o maximo para o sorteio
+
+    private float X, Y;
+
+    public float RangeMinX, RangeMaxX, RangeMinY, RangeMaxY;
+    void Start()
+    {
+        X = Random.Range(RangeMinX, RangeMaxX);
+        Y = Random.Range(RangeMinY, RangeMaxY);
+        LugarDoSpawn = new Vector3(X, Y, 0f);
+        int quantidade = Random.Range(minimoDeInimigos, maximoDeInimigos); // aqui acontece o sorteio da quantidade de inimigos
+        for (int x = 0; x < quantidade; x++)
+        {
+            Instantiate(inimigo, LugarDoSpawn, transform.rotation); // instancia um inimigo aleatorio
+        }
+    }
+}
